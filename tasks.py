2dd9ce5be59e121a5ad032d27268d48018769025
Serang <gisoft@hanmail.net>
2017-11-01 06:25:58 +0900

    아이디 수정
    아이디 및 서버 수정

--- tasks.py
+++ tasks.py
@@ -12,48 +12,54 @@ Content-Type:application/json
 Accept:application/json
 """
 
+
 # https://api.korbit.co.kr/v1/ticker
 
 def echo_response(message):
-  print(message)
-  
-  if message["type"] == "message":
-    if "bitcoin" in message["text"]:
+    print(message)
 
-      r = requests.get("https://api.korbit.co.kr/v1/ticker")
-      bitcoin_price = r.json()["last"]
-      msg = "bitcoin price is %s" % bitcoin_price
-      print(msg)
-      ReplyToActivity(fill=message,
-                    text=msg).send()
-    else:
-      data =  {
-        "documents": [
-          {
-            "language": "en",
-            "id": "1",
-            "text": message["text"]
-          }
-        ]
-      }
-      headers = {'Ocp-Apim-Subscription-Key': '4cfe6f744f1b486db3fa83d874bafdd9',
-                 'Content-Type': 'application/json',
-                 'Accept': 'application/json',
-                 }
+    if message["type"] == "message":
+        if "bitcoin" in message["text"]:
 
-      r = requests.post("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
-                        data=json.dumps(data),
-                        headers=headers)
-      emo_score = r.json()["documents"][0]["score"]
-      msg = "emotion score is %s\n" % emo_score
+            r = requests.get("https://api.korbit.co.kr/v1/ticker")
+            bitcoin_price = r.json()["last"]
+            msg = "bitcoin price is %s" % bitcoin_price
+            print(msg)
+            #ReplyToActivity(fill=message, text=msg).send()
+        else:
+            data = {
+                "documents": [
+                    {
+                        "language": "en",
+                        "id": "81a86d6e-8ce1-4582-932d-b01c4c1adc64",
+                        "text": message["text"]
+                    }
+                ]
+            }
+            headers = {'Ocp-Apim-Subscription-Key': 'a4828d6a9e65477492febf25736aa2b2',
+                       'Content-Type': 'application/json',
+                       'Accept': 'application/json',
+                       }
 
-      if emo_score > 0.5:
-        msg = msg + "You look happy!"
-      else:
-        msg = msg + "You look unhappy.."
+            r = requests.post("https://eastus2.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
+                              data=json.dumps(data),
+                              headers=headers)
+            print( r.json() )
+            emo_score = r.json()["documents"][0]["score"]
+            msg = "emotion score is %s\n" % emo_score
 
-      print(msg)
+            if emo_score > 0.5:
+                msg = msg + "You look happy!"
+            else:
+                msg = msg + "You look unhappy.."
 
+            print(msg)
 
-      ReplyToActivity(fill=message,
-                      text=msg).send()
+            #ReplyToActivity(fill=message, text=msg).send()
+
+message = { "text": "bitcoin", "type": "message" }
+echo_response(message)
+
+message = { "text": "I am happy", "type": "message" }
+echo_response(message)
+
