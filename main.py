b90aee3acb5799569b059323799792b507f91221
Serang <gisoft@hanmail.net>
2017-11-02 03:38:49 +0900

    cc

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from tasks import *
 import os
 
 #bot = MsBot(port=int(os.environ['PORT']))
-bot = MsBot(port=5000)
-#bot = MsBot()
+#bot = MsBot(port=5000)
+bot = MsBot()
 bot.add_process(echo_response)
 
 if __name__ == '__main__':
